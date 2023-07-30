0c5062f7989b1b2aab23b7ab988fa27776cce025
HJVDT <hjvdtuin@gmail.com>
2023-07-30 18:36:45 -0300

    tercer preentrega.Modifica a Dom y localstorage.

--- js/turnos.js
+++ js/turnos.js
@@ -45,52 +45,26 @@ let arregloHorarios = [
 ];
 
 let contadorId = 1;
-let respuesta = true;
 
-while (respuesta != "salir" && respuesta) {
-    respuesta = mostrarMenu();
-}
+document.addEventListener("DOMContentLoaded", () => {
+    cargarTurnosDesdeLocalStorage();
+});
 
-function mostrarMenu() {
-    let flag = true;
-    while (flag) {
-        let mensaje = "Indique lo que desea hacer: ";
-        mensaje += "\n1) Ver horarios disponibles ";
-        mensaje += "\n2) Agendar un turno ";
-        mensaje += "\n3) Cancelar un turno ";
-        mensaje += "\n4) Ver los turnos dados ";
-        mensaje += "\n5) Salir ";
-        let resp = prompt(mensaje);
-        switch (resp) {
-            case "1":
-                alert("Los horarios disponibles son:\n" + mostrarHorarios());
-                break;
-            case "2":
-                agregarTurno();
-                break;
-            case "3":
-                cancelarTurno();
-                break;
-            case "4":
-                alert("Los turnos son:\n" + mostrarTurnos());
-                break;
-            case "5":
-                alert("Gracias por utilizar nuestra pagina :) ");
-                flag = false;
-                break;
-            case null:
-                alert("Gracias por utilizar nuestra pagina :) ");
-                flag = false;
-                break;
-            default:
-                alert("No ingreso una opcion valida");
+function mostrarHorarios() {
+    let listaHorarios = "";
+    if (arregloHorarios.length === 0) {
+        listaHorarios = "No hay horarios.";
+    } else {
+        for (let i = 0; i < arregloHorarios.length; i++) {
+            listaHorarios += "\n" + arregloHorarios[i];
         }
     }
+    mostrarMensaje(listaHorarios);
 }
 
 function agregarTurno() {
     let nombrePaciente = prompt("Ingrese el nombre del paciente:");
-    let fecha = prompt("Ingrese la fecha del turno, sólo de Lunes a Viernes(formato: DD/MM/AAAA):");
+    let fecha = prompt("Ingrese la fecha del turno, sólo de Lunes a Viernes (formato: DD/MM/AAAA):");
     let hora = prompt("Ingrese la hora del turno (formato hh:mm):");
 
     // Verifico si la fecha y hora del turno son válidas
@@ -99,12 +73,12 @@ function agregarTurno() {
 
     if (!fechaValida.test(fecha) || !horaValida.test(hora)) {
         alert("La fecha u hora del turno ingresados no son válidos.");
-        agregarTurno();
         return;
     }
     arregloTurnos.push(new Turno(contadorId, nombrePaciente, fecha, hora));
     contadorId++;
-    alert("Turno agendado correctamente.\n" + mostrarTurnos());
+    guardarTurnosEnLocalStorage();
+    mostrarTurnos();
 }
 
 function cancelarTurno() {
@@ -113,7 +87,8 @@ function cancelarTurno() {
 
     if (indice >= 0) {
         arregloTurnos.splice(indice, 1);
-        alert("Turno cancelado correctamente.\n" + mostrarTurnos());
+        guardarTurnosEnLocalStorage();
+        mostrarTurnos();
     } else {
         alert("No se encontró un turno con el ID especificado.");
     }
@@ -128,19 +103,7 @@ function buscarTurno(idTurno) {
     return -1;
 }
 
-function mostrarHorarios() {
-    let listaHorarios = "";
-    if (arregloHorarios.length === 0) {
-        listaHorarios = "No hay horarios.";
-    } else {
-        for (let i = 0; i < arregloHorarios.length; i++) {
-            listaHorarios += "\n" + arregloHorarios[i];
-        }
-    }
-    return listaHorarios;
-}
-
-function mostrarTurnos(orden) {
+function mostrarTurnos() {
     let listaTurnos = "";
     if (arregloTurnos.length === 0) {
         listaTurnos = "No hay turnos agendados.";
@@ -165,5 +128,30 @@ function mostrarTurnos(orden) {
     for (let i = 0; i < arregloTurnos.length; i++) {
         listaTurnos += "\n" + arregloTurnos[i].mostrarInformacion();
     }
-return listaTurnos;
+    mostrarMensaje(listaTurnos);
+}
+
+function mostrarMensaje(mensaje) {
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerText = mensaje;
+}
+
+function guardarTurnosEnLocalStorage() {
+    localStorage.setItem("turnos", JSON.stringify(arregloTurnos));
+}
+
+function cargarTurnosDesdeLocalStorage() {
+    const turnosGuardados = localStorage.getItem("turnos");
+    if (turnosGuardados) {
+        arregloTurnos = JSON.parse(turnosGuardados).map(
+            (turno) => new Turno(turno.id, turno.nombrePaciente, turno.fecha, turno.hora)
+        );
+        contadorId = arregloTurnos.length + 1;
+    }
+}
+
+function salir() {
+    alert("Gracias por utilizar nuestra página :)");
+    guardarTurnosEnLocalStorage();
+    window.location.href = '../index.html';
 }
